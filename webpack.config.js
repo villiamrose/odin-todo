e3a5bff59cc00c9e03171fc3f2c4efeacc30f826
villiamrose <villiamrose@live.com>
2023-04-22 22:00:11 +0800

    Setup and initialize working environment

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/scripts/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: './src'
+  },
+  watchOptions: {
+    poll: 500,
+    ignored: './node_modules'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.jpg/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
